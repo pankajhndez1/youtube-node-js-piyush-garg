8e65e592e8bbc1614d5b91b62b7f9dc42777b85d
pankaj kumar <pankajkumar12861@gmail.com>
2025-04-01 17:00:32 +0530

    sync and async opertaions

---
+++ syncAsyncoperations.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+
+// Asynchronous file write
+console.log("1");
+
+fs.readFile("hello.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err, "error in reading file");
+  } else {
+    console.log(data, "data in async");
+  }
+});
+
+console.log("2");
+console.log("3");
+console.log("4");
+
+
+
+
+
+// Synchronous file write
+console.log('a')
+console.log('b')
+const syncData= fs.readFileSync('hello.txt','utf-8')
+console.log(syncData,'syncData')
+console.log('c')
+console.log('d')
